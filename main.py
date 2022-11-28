c0c78bcfd6f95c7886e5c950c02d6a3d6bb79085
Lin GuanBin <35720323+kiethrios@users.noreply.github.com>
2022-11-28 17:24:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def goal():
     turn_left()
         
     while not at_goal():
-        if front_is_clear():
-            move()
-        elif right_is_clear():
+        if right_is_clear():
             turn_right()
+            move()
+        elif front_is_clear():
+            move()
         else:
             turn_left()
 goal()
